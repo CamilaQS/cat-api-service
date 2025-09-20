ad93a205182072cbfa4604b98ef443177f10dd42
CamilaQS <camila.q.s@hotmail.com>
2025-09-20 10:35:39 -0300

    Estrutura do projeto + configuracoes do properties

---
+++ src/main/java/com/itau/cat_api/CatApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.itau.cat_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/cat_api/CatApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.cat_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
